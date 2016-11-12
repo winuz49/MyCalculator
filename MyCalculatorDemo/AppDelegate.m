4269da1b8f2b9a4da4f61853ba97790cce4b769e
wzj <wzj@promote.cache-dns.local>
2016-11-13 00:38:19 +0800

    add view navigation

--- MyCalculatorDemo/AppDelegate.m
+++ MyCalculatorDemo/AppDelegate.m
@@ -21,29 +21,29 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    _window=[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
-    // 获得storyboard对象
-    UIStoryboard *mainStoryBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    
-    ViewController *calViewController=[mainStoryBoard instantiateViewControllerWithIdentifier:@"Calculator"];
-    WeightViewController *weightViewController=[mainStoryBoard instantiateViewControllerWithIdentifier:@"WeightAnalyse"];
-    GalleryTabViewController *galleryTabViewController=[[GalleryTabViewController alloc]init ];
-   
-   
-    
-    UITabBarItem *settingItem = [[UITabBarItem alloc] initWithTitle:@"计算器" image:[UIImage imageNamed: @""] tag:5];
-    calViewController.tabBarItem = settingItem;
-     UINavigationController *navigationController=[[UINavigationController alloc] initWithRootViewController:calViewController];
-    galleryTabViewController.viewControllers=@[navigationController,weightViewController];
-    
-    
-    //注意默认情况下UITabBarController在加载子视图时是懒加载的，所以这里调用一次contactController，否则在第一次展示时只有第一个控制器tab图标，contactController的tab图标不会显示
-    for (UIViewController *controller in galleryTabViewController.viewControllers) {
-        UIViewController * view= controller.view;
-    }
-    
-    _window.rootViewController=galleryTabViewController;
-    [_window makeKeyAndVisible];
+//    _window=[[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+//    // 获得storyboard对象
+//    UIStoryboard *mainStoryBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+//    
+//    ViewController *calViewController=[mainStoryBoard instantiateViewControllerWithIdentifier:@"Calculator"];
+//    WeightViewController *weightViewController=[mainStoryBoard instantiateViewControllerWithIdentifier:@"WeightAnalyse"];
+//    GalleryTabViewController *galleryTabViewController=[[GalleryTabViewController alloc]init ];
+//   
+//   
+//    
+//    UITabBarItem *settingItem = [[UITabBarItem alloc] initWithTitle:@"计算器" image:[UIImage imageNamed: @""] tag:5];
+//    calViewController.tabBarItem = settingItem;
+//     UINavigationController *navigationController=[[UINavigationController alloc] initWithRootViewController:calViewController];
+//    galleryTabViewController.viewControllers=@[navigationController,weightViewController];
+//    
+//    
+//    //注意默认情况下UITabBarController在加载子视图时是懒加载的，所以这里调用一次contactController，否则在第一次展示时只有第一个控制器tab图标，contactController的tab图标不会显示
+//    for (UIViewController *controller in galleryTabViewController.viewControllers) {
+//        UIViewController * view= controller.view;
+//    }
+//    
+//    _window.rootViewController=galleryTabViewController;
+//    [_window makeKeyAndVisible];
     
     
     
